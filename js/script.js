5a39e26873d11e00dc20cc5bd6a589e559e5b845
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-02 16:04:20 +0200

    (UPD) 2 point

--- js/script.js
+++ js/script.js
@@ -24,5 +24,11 @@
 //punto 1
 // Scrivi un programma che stampi i numeri da 1 a 100,
 for (i=0 ; i<=100 ; ++i) {
-  console.log(i)
+ //punto 2
+
+  // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+  if(i % 3 === 0 && i % 5 === 0) {
+    console.log( `FizzBuzz`)
+
+
 }
